32a6d43bfa7041c6f7c52189e3fc5a7514c1679c
Hirotaka Mizutani <hirotaka@mizutani.to>
2014-08-02 15:56:39 +0900

    Use default rbenv cookbook

--- Berksfile
+++ Berksfile
@@ -1,11 +1,11 @@
-site :opscode
+source "https://api.berkshelf.com"
 
 metadata
 cookbook 'apt'
 cookbook 'git'
 cookbook 'ruby_build'
-cookbook 'rbenv', github: "fnichol/chef-rbenv", ref: "v0.7.2"
-# cookbook 'rbenv', github: "fnichol/chef-rbenv"
+#cookbook 'rbenv', github: "fnichol/chef-rbenv", ref: "v0.7.2"
+cookbook 'rbenv'
 cookbook 'imagemagick'
 cookbook 'mysql'
 cookbook 'zsh'
--- recipes/rbenv.rb
+++ recipes/rbenv.rb
@@ -38,64 +38,12 @@ when 'ubuntu', 'debian'
   end
 end
 
-rbenv = {}
-rbenv['rubies'] = ['1.9.3-p484']
-rbenv['global'] = '1.9.3-p484'
-rbenv['gems']   = {
-  '1.9.3-p484' => [
-    { 'name' => 'bundler' }
-  ]
-}
-
-include_recipe "rbenv::system_install"
-
-Array(rbenv['rubies']).each do |rubie|
-  if rubie.is_a?(Hash)
-    rbenv_ruby rubie['name'] do
-      environment rubie['environment'] if rubie['environment']
-    end
-  else
-    rbenv_ruby rubie
-  end
+include_recipe "rbenv::default"
+include_recipe "rbenv::ruby_build"
+rbenv_ruby "1.9.3-p484" do
+  global true
 end
 
-if rbenv['global']
-  rbenv_global rbenv['global']
-end
-
-rbenv['gems'].each_pair do |rubie, gems|
-  Array(gems).each do |gem|
-    rbenv_gem gem['name'] do
-      rbenv_version rubie
-
-      %w{version action options source}.each do |attr|
-        send(attr, gem[attr]) if gem[attr]
-      end
-    end
-  end
-end
-
-# staff group member can install ruby via rbenv
-directory "/usr/local/rbenv" do
-  owner "root"
-  group "staff"
-  mode "2775"
-  recursive true
-end
-
-Dir[ "/usr/local/rbenv/**/*" ].each do |path|
-  if  File.directory?(path)
-    directory path do
-      owner "root"
-      group "staff"
-      mode "775"
-    end
-  end
-end
-
-# Add vagrant user to staff group
-group "staff" do
-  action :modify
-  members "vagrant"
-  append true
+rbenv_gem "bundler" do
+  ruby_version "1.9.3-p484"
 end
